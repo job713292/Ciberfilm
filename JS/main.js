6d231255673d7758bb1c5f076ab99a09d7bbdd39
Job_Nuñez <jobnunez713@gmail.com>
2025-05-13 18:59:35 -0500

    Footer arreglo

--- JS/main.js
+++ JS/main.js
@@ -12,3 +12,26 @@ boton_izquierda.addEventListener("click", function() {
 boton_derecha.addEventListener("click", function() {
     slider.scrollLeft += 500;
 });
+
+ let carrito = [];
+
+    function agregarAlCarrito(nombre, precio) {
+      carrito.push({ nombre, precio });
+      alert(`${nombre} agregado al carrito.`);
+    }
+
+    function mostrarCarrito() {
+      let lista = document.getElementById('listaCarrito');
+      let total = document.getElementById('total');
+      let contenedor = document.getElementById('carrito');
+      lista.innerHTML = '';
+      let suma = 0;
+      carrito.forEach(producto => {
+        const item = document.createElement('li');
+        item.textContent = `${producto.nombre} - $${producto.precio}`;
+        lista.appendChild(item);
+        suma += producto.precio;
+      });
+      total.textContent = `Total: $${suma}`;
+      contenedor.style.display = 'block';
+    }
