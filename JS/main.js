11c6d2dcd217000f4d9cbdd84881319f426b924b
Job_Nuñez <jobnunez713@gmail.com>
2025-06-04 16:49:24 -0500

    Boton flotante de whatsapp

--- JS/main.js
+++ JS/main.js
@@ -4,34 +4,34 @@ let boton_derecha = document.getElementById("boton-derecha");
 
 
 //Deplazamientio a la izquierda
-boton_izquierda.addEventListener("click", function() {
-    slider.scrollLeft -= 500;
+boton_izquierda.addEventListener("click", function () {
+  slider.scrollLeft -= 500;
 });
 
 //Deplazamiento a la derecha
-boton_derecha.addEventListener("click", function() {
-    slider.scrollLeft += 500;
+boton_derecha.addEventListener("click", function () {
+  slider.scrollLeft += 500;
 });
 
- let carrito = [];
+let carrito = [];
 
-    function agregarAlCarrito(nombre, precio) {
-      carrito.push({ nombre, precio });
-      alert(`${nombre} agregado al carrito.`);
-    }
+function agregarAlCarrito(nombre, precio) {
+  carrito.push({ nombre, precio });
+  alert(`${nombre} agregado al carrito.`);
+}
 
-    function mostrarCarrito() {
-      let lista = document.getElementById('listaCarrito');
-      let total = document.getElementById('total');
-      let contenedor = document.getElementById('carrito');
-      lista.innerHTML = '';
-      let suma = 0;
-      carrito.forEach(producto => {
-        const item = document.createElement('li');
-        item.textContent = `${producto.nombre} - $${producto.precio}`;
-        lista.appendChild(item);
-        suma += producto.precio;
-      });
-      total.textContent = `Total: $${suma}`;
-      contenedor.style.display = 'block';
-    }
+function mostrarCarrito() {
+  let lista = document.getElementById('listaCarrito');
+  let total = document.getElementById('total');
+  let contenedor = document.getElementById('carrito');
+  lista.innerHTML = '';
+  let suma = 0;
+  carrito.forEach(producto => {
+    const item = document.createElement('li');
+    item.textContent = `${producto.nombre} - $${producto.precio}`;
+    lista.appendChild(item);
+    suma += producto.precio;
+  });
+  total.textContent = `Total: $${suma}`;
+  contenedor.style.display = 'block';
+}
