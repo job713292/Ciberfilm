1ff1757dd532a1d77533c727f5e5ae0a624b6cd3
Job <jobnunez713@gmail.com>
2025-06-16 12:19:08 -0500

    Responsive-1

--- JS/main.js
+++ JS/main.js
@@ -35,3 +35,52 @@ function mostrarCarrito() {
   total.textContent = `Total: $${suma}`;
   contenedor.style.display = 'block';
 }
+
+//mostrar productos slider 
+
+const productos = JSON.parse(localStorage.getItem("productos")) || [];
+const contenedor = document.getElementById("slider");
+const mostrarEliminar = window.location.pathname.includes("agregarProductos.html");
+const mostrarAgregar = !mostrarEliminar;
+
+if (productos.length === 0) {
+  contenedor.innerHTML = "<p>No hay productos guardados.</p>";
+} else {
+  productos.forEach((producto, index) => {
+    const tarjeta = document.createElement("div");
+    tarjeta.classList.add("producto-1");
+
+    tarjeta.innerHTML = `
+  <div class="p-s">
+    <img src="${producto.imagen}" alt="${producto.nombre}">
+  </div>
+  <h1>${producto.nombre}</h1>
+  <p>$${producto.precio}</p>
+  <div class="p-s-1">
+    ${mostrarAgregar ? `<button onclick="agregarAlCarrito('${producto.nombre}', ${producto.precio})">AGREGAR</button>` : ""}
+    ${mostrarEliminar ? `<button onclick="eliminarProducto(${index})">ELIMINAR</button>` : ""}
+  </div>
+`;
+    contenedor.appendChild(tarjeta);
+  });
+}
+
+function agregarAlCarrito(nombre, precio) {
+  alert(`Producto agregado: ${nombre} ($${precio})`);
+}
+
+function eliminarProducto(index) {
+  productos.splice(index, 1);
+  localStorage.setItem("productos", JSON.stringify(productos));
+  location.reload(); // Recarga para reflejar los cambios
+}
+
+
+// Menu responsive
+const toggle = document.getElementById('menu-toggle');
+const lista = document.querySelector('.lista');
+
+toggle.addEventListener('click', () => {
+  lista.classList.toggle('active');
+});
+
